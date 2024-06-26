c88fcc46931a9d209b92d776e76d1877668ef06e
patelshaunak5098 <patelshaunak5098@gmail.com>
2024-06-26 15:27:49 -0400

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,12 @@ docker login -u patelshaunak5098 -p Shaunak@5098
 # Pull the latest image from Docker Hub
 docker pull patelshaunak5098/my-docker-project2:latest
 
+CONTAINER_ID=$(docker ps -q --filter ancestor=patelshaunak5098/my-docker-project2)
+
+echo "container ID ====== $CONTAINER_ID"
 # Stop and remove the old container if it exists
-docker stop my-docker-project2 || true
-docker rm my-docker-project2 || true
+docker stop $CONTAINER_ID
+docker rm $CONTAINER_ID
 
 # Run the new container
 docker run -d --name my-docker-project2 -p 80:3000 patelshaunak5098/my-docker-project2:latest
