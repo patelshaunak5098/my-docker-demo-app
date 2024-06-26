047a9cafd60217004c8a183a7f0a6d5a357acdc9
patelshaunak5098 <patelshaunak5098@gmail.com>
2024-06-26 15:20:48 -0400

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd /home/ubuntu/app
 # Log in to Docker Hub
-docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
+docker login -u patelshaunak5098 -p Shaunak@5098
 
 # Pull the latest image from Docker Hub
-docker pull $DOCKERHUB_USERNAME/my-docker-project2:latest
+docker pull patelshaunak5098/my-docker-project2:latest
 
 # Stop and remove the old container if it exists
 docker stop my-docker-project2 || true
 docker rm my-docker-project2 || true
 
 # Run the new container
-docker run -d --name my-docker-project2 -p 80:3000 $DOCKERHUB_USERNAME/my-docker-project2:latest
+docker run -d --name my-docker-project2 -p 80:3000 patelshaunak5098/my-docker-project2:latest
